48011df170364b0c08277709276c12d7d81aa4bc
Ruben-hub-blip <rubenarangomiranda@gmail.com>
2026-03-10 16:17:24 +0000

    actualización del backend y conexión con API de ubicaciones

--- src/db.js
+++ src/db.js
@@ -1,14 +1,11 @@
-import dotenv from "dotenv";
-dotenv.config(); 
-
 import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false,
-  },
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
--- src/routes/barrio.routes.js
+++ src/routes/barrio.routes.js
@@ -3,27 +3,25 @@ import pool from "../db.js";
 
 const router = express.Router();
 
-router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-
+router.get("/", async (req, res) => {
   try {
-    const result = await pool.query(
-      `
+
+    const result = await pool.query(`
       SELECT 
         b.id,
-        b.nombre AS barrio,
+        b.nombre,
+        b.latitud,
+        b.longitud,
         l.nombre AS localidad
       FROM barrios b
       JOIN localidades l ON b.id_localidad = l.id
-      WHERE b.id = $1
-      `,
-      [id]
-    );
+      ORDER BY b.nombre
+    `);
 
-    res.json(result.rows[0]);
+    res.json(result.rows);
 
   } catch (error) {
-    console.error("ERROR REAL:", error);
+    console.error(error);
     res.status(500).json({ error: error.message });
   }
 });
